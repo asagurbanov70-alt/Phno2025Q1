dfc4941f1453b9024206d646b3981313ab8aa65c
Гульнара <kjkjkjk@kjjk>
2025-09-25 18:34:30 +0300

    add: first function

---
+++ function.js
@@ -0,0 +1,5 @@
+const plus=(firstNumber,secondNumber)=>{
+ return firstNumber+secondNumber
+}
+
+console.log(plus(5,7))
---
+++ var.js
@@ -0,0 +1,6 @@
+const greeting ="привет мир"
+console.log(greeting)
+const firstNumber = 5
+const secondNumber = 3
+const result = firstNumber*secondNumber
+console.log(result)
